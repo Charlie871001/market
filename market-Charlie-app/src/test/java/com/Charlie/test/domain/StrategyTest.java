76c5229a67939b1556b681c6d1122e7fa1aa437d
Charlie871001 <mahui871001@gmail.com>
2025-05-22 10:10:03 +0800

    连接redis

---
+++ market-Charlie-app/src/test/java/com/Charlie/test/domain/StrategyTest.java
@@ -0,0 +1,44 @@
+package com.Charlie.test.domain;
+
+import com.Charlie.infrastructure.redis.IRedisService;
+import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.units.qual.K;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.redisson.api.RMap;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Charlie
+ * @description 策略领域测试
+ * @title StrategyTest
+ * @date 2025/5/22 9:43
+ **/
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StrategyTest {
+
+    @Resource
+    private IRedisService redisService;
+
+    @Test
+    public void test_map(){
+        RMap<Integer, Integer> map = redisService.getMap("strategy_id_100001");
+        map.put(1, 101);
+        map.put(2, 101);
+        map.put(3, 101);
+        map.put(4, 102);
+        map.put(5, 102);
+        map.put(6, 102);
+        map.put(7, 103);
+        map.put(8, 103);
+        map.put(9, 104);
+        map.put(10, 105);
+
+        log.info("测试结果：{}", redisService.getMap("strategy_id_100001").get(1));
+    }
+}
