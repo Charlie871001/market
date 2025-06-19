61b8760cb7a24473f639b6b4e4dabeab350a2bae
Charlie871001 <mahui871001@gmail.com>
2025-06-19 21:53:17 +0800

    抽奖中置规则过滤

--- market-Charlie-domain/src/main/java/com/Charlie/domain/strategy/model/entity/StrategyEntity.java
+++ market-Charlie-domain/src/main/java/com/Charlie/domain/strategy/model/entity/StrategyEntity.java
@@ -39,7 +39,11 @@ public class StrategyEntity {
     }
 
     public String getRuleWeight() {
-        for (String ruleModel : this.ruleModels()) {
+        String[] ruleModels = this.ruleModels();
+        if (null == ruleModels) { // 必须做判空处理
+            return null;
+        }
+        for (String ruleModel : ruleModels) {
             if ("rule_weight".equals(ruleModel)) {
                 return ruleModel;
             }
